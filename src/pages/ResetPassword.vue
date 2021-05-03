9c7c797bc23930437cbb2be4da33c596a0641c62
Dario Salvi <dariosalvi78@gmail.com>
2021-05-03 14:16:50 +0200

    fixing password reset ui

--- src/pages/ResetPassword.vue
+++ src/pages/ResetPassword.vue
@@ -3,22 +3,55 @@
     <q-page-container>
       <q-page class="flex flex-center">
         <q-card class="q-pa-sm loginBox">
-          <q-card-title v-show="!email">Reset your password</q-card-title>
-          <q-card-title v-show="email">Reset password for {{ email }}</q-card-title>
-          <q-card-main>
-            <q-field label="Token" helper="As received on your email." :error="$v.token.$error" error-message="A token is required." >
-              <q-input v-model.trim="$v.token.$model" type="text" @blur.native="$v.token.$touch"/>
-            </q-field>
-            <q-field class="q-mt-md" label="Password" helper="Enter a new password." :error="$v.newPassword.$error" :error-message="getFirstPwdCheckError(newPassword)">
-              <q-input v-model.trim="$v.newPassword.$model" type="password" @blur.native="$v.newPassword.$touch"/>
-            </q-field>
-            <q-field class="q-mt-md" label="Repeat Password" helper="Please confirm your password." :error="$v.newPassword2.$error" error-message="Passwords must match">
-              <q-input v-model.trim="$v.newPassword2.$model" type="password" @blur.native="$v.newPassword2.$touch"/>
-            </q-field>
-          </q-card-main>
+          <q-card-section>
+            <div
+              class="text-h6"
+              v-show="!email"
+            >Reset your password</div>
+            <div
+              class="text-h6"
+              v-show="email"
+            >Reset password for {{ email }}</div>
+            <form autocomplete="off">
+              <q-input
+                label="Token"
+                :error="$v.token.$error"
+                error-message="A token is required."
+                v-model.trim="$v.token.$model"
+                @blur.native="$v.token.$touch"
+                type="textarea"
+              />
+              <q-input
+                class="q-mt-md"
+                v-model.trim="$v.newPassword.$model"
+                type="password"
+                @blur.native="$v.newPassword.$touch"
+                label="New password"
+                :error="$v.newPassword.$error"
+                :error-message="getFirstPwdCheckError(newPassword)"
+              />
+              <q-input
+                v-model.trim="$v.newPassword2.$model"
+                type="password"
+                @blur.native="$v.newPassword2.$touch"
+                class="q-mt-md"
+                label="Repeat new password"
+                :error="$v.newPassword2.$error"
+                error-message="Passwords must match"
+              />
+            </form>
+          </q-card-section>
           <q-card-actions>
-            <q-btn label="Cancel" color="secondary" @click="$router.push('login')"/>
-            <q-btn label="Reset" color="primary" @click="resetUserPassword"/>
+            <q-btn
+              label="Cancel"
+              color="secondary"
+              @click="$router.push('login')"
+            />
+            <q-btn
+              label="Reset"
+              color="primary"
+              @click="resetUserPassword"
+            />
           </q-card-actions>
         </q-card>
       </q-page>
@@ -61,7 +94,6 @@ export default {
   name: 'ResetPasswordPage',
   data () {
     return {
-      email: undefined,
       token: '',
       newPassword: '',
       newPassword2: ''
@@ -70,11 +102,12 @@ export default {
   validations: {
     token: { required },
     newPassword: { required, checkPwdStrength },
-    newPassword2: { sameAsPassword: sameAs('pw1') }
+    newPassword2: { sameAsPassword: sameAs('newPassword') }
   },
   created () {
     if (this.$route.query.email) this.email = this.$route.query.email
     if (this.$route.query.token) this.token = this.$route.query.token
+    console.log(this.token)
   },
   methods: {
     getFirstPwdCheckError (pwd) {
@@ -104,10 +137,9 @@ export default {
       }
     },
     async resetUserPassword () {
-      this.$v.email.$touch()
       this.$v.newPassword.$touch()
       this.$v.newPassword2.$touch()
-      if (this.$v.email.$error || this.$v.newPassword.$error || this.$v.newPassword2.$error) {
+      if (this.$v.newPassword.$error || this.$v.newPassword2.$error) {
         this.$q.notify('Please correct the indicated fields.')
       } else {
         try {
@@ -115,17 +147,16 @@ export default {
 
           this.$q.dialog({
             title: 'New password set',
-            message: 'Now you need to login.',
+            message: 'You can now login with your new password.',
             ok: true,
             cancel: false,
             preventClose: true
-          }).then(() => {
-            this.$router.push('/login')
           })
         } catch (error) {
+          let errmsg = error.response && error.response.data ? error.response.data : error.message
           this.$q.notify({
             color: 'negative',
-            message: 'Cannot change password: ' + error.message,
+            message: 'Cannot change password: ' + errmsg,
             icon: 'report_problem'
           })
         }
